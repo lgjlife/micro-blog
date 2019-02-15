2a94385ace6a61a075899d11a797a5e582a8b11b
lgjlife <liang_gj17@163.com>
2019-02-15 17:31:50 +0800

    elaticsearch can search

---
+++ microblog-search/src/main/java/com/clolud/microblog/MicroBlogResearchApplication.java
@@ -0,0 +1,14 @@
+package com.clolud.microblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBlogResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroBlogResearchApplication.class, args);
+    }
+
+}
+
---
+++ microblog-search/src/main/java/com/clolud/microblog/pojo/Book.java
@@ -0,0 +1,15 @@
+package com.clolud.microblog.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Data
+public class Book {
+    private  String title;
+    private  String author;
+    private  String wordCount;
+
+}
