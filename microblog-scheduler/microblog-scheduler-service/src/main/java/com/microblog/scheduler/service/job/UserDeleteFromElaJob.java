942d630b92667b7b538b8f03d96e40e23f4acb34
lgjlife <liang_gj17@163.com>
2019-05-17 14:51:44 +0800

    任务调度中心添加ela博客删除和用户删除定时任务

---
+++ microblog-scheduler/microblog-scheduler-service/src/main/java/com/microblog/scheduler/service/job/UserDeleteFromElaJob.java
@@ -0,0 +1,40 @@
+package com.microblog.scheduler.service.job;
+
+import com.microblog.scheduler.service.message.producer.MqProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+/**
+ * @program: top-parent
+ * @description:
+ * @author: Mr.lgj
+ * @version:
+ * @See:
+ * @create: 2018-11-20 11:34
+ **/
+
+@Slf4j
+public class UserDeleteFromElaJob extends AbstractJob implements Job {
+
+    @Autowired
+    private MqProducer mqProducer;
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+        log.info("UserDeleteFromElaJob 任务正在运行....，" + "当前时间 = " + new Date().toString());
+        try{
+            mqProducer.asyncSend("message.ela","user.delete.request","");
+        }
+        catch(Exception ex){
+            log.error(ex.getMessage());
+        }
+
+
+    }
+}
