31338bc45f62f8bb45ad0b5e72b78cfa9c46cac0
lgjlife <liang_gj17@163.com>
2019-06-25 02:38:36 +0800

    使用新的cloud网关spring.cloud.gatway实现websocket路由

---
+++ microblog-gate/src/main/java/com/microblog/microbloggate/MicroblogGateApplication.java
@@ -0,0 +1,13 @@
+package com.microblog.microbloggate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblogGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroblogGateApplication.class, args);
+    }
+
+}
---
+++ microblog-websocket/src/main/java/com/microblog/websocket/MicroblogWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.microblog.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblogWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroblogWebsocketApplication.class, args);
+    }
+
+}
